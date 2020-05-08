9dd21f1a5b0880dc86dd8622bf88841a52a435e0
Amit Mittal <mittalmailbox@gmail.com>
2020-05-09 05:10:16 +0530

    Removed History

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,16 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import { createHashHistory } from 'history';
 import Root from './containers/Root';
 
 import './app.global.css';
-
-const history = createHashHistory();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 document.addEventListener('DOMContentLoaded', () =>
   render(
     <AppContainer>
-      <Root history={history} />
+      <Root />
     </AppContainer>,
     document.getElementById('root')
   )
